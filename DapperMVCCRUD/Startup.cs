1f1130f06063ed1998897267b0b914bec8184005
AAHA <AAHA@AAHA1>
2019-12-24 12:32:23 +0500

    new changes

---
+++ DapperMVCCRUD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DapperMVCCRUD.Startup))]
+namespace DapperMVCCRUD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
